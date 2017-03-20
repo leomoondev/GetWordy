1b9afe2b0405f5c0cc44920d64c5906793728e7d
Leo Moon <leomoondev@gmail.com>
2017-03-19 23:03:05 -0700

    first commit

---
+++ GetWordy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GetWordy
+//
+//  Created by Hyung Jip Moon on 2017-03-19.
+//  Copyright © 2017 leomoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
