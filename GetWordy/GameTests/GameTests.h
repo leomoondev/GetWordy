7741411dab37d5c3170fb644d18cefb51ab514b5
Leo Moon <leomoondev@gmail.com>
2017-03-21 22:57:58 -0700

    Add game tests for unit testing

---
+++ GetWordy/GameTests/GameTests.h
@@ -0,0 +1,13 @@
+//
+//  GameTests.h
+//  GameTests
+//
+//  Created by Hyung Jip Moon on 2013-11-05.
+//  Copyright (c) 2013 hyungjip. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface GameTests : SenTestCase
+
+@end
---
+++ GetWordy/GameTests/GameTests.m
@@ -0,0 +1,32 @@
+//
+//  GameTests.m
+//  GameTests
+//
+//  Created by Hyung Jip Moon on 2013-11-05.
+//  Copyright (c) 2013 hyungjip. All rights reserved.
+//
+
+#import "GameTests.h"
+
+@implementation GameTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in GameTests");
+}
+
+@end
